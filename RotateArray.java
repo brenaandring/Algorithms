fdf2b7679e189e03aa0edf7efc0354acaf6bb21c
Brena Andring <bzandring@gmail.com>
2020-02-22 12:23:28 -0800

    leetcode rotate array

---
+++ RotateArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        for(int i = 0; i <k; i ++) {
+            shiftByOne(nums);
+        }
+    }
+
+    private void shiftByOne(int[] nums) {
+        if (nums.length < 2) {
+            return;
+        }
+        int prev = nums[0];
+        for(int i = 0 ; i < nums.length ; i++){
+            int tmp = nums[i];
+            nums[i] = prev;
+            prev = tmp;
+        }
+        nums[0]=prev;
+    }
+}
