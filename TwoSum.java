d9baf4da30ba5f7bd47c39364b4c15147ef09735
Brena Andring <bzandring@gmail.com>
2020-02-22 12:18:10 -0800

    leetcode two sum

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for(int x=0; x < nums.length; x++) {
+                if(i != x) {
+                    int sum = nums[i] + nums[x];
+                    if(sum == target) {
+                        int[] result = new int[]{i, x};
+                        return result;
+                    }
+                }
+            }
+        }
+        return new int[0];
+    }
+}
