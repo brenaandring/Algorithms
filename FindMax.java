a23e2b66725cea8992a5a62572a4e1eab3aba69e
Brena Andring <bzandring@gmail.com>
2020-03-03 20:11:58 -0800

    basic 13 algo

---
+++ FindMax.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class FindMax {
+    public static double findMax(double[] a) {
+        double max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        double[] arr = {23, 123, 4, 213421, -1234, 1234, 9000000.0, 1};
+        double max = findMax(arr);
+        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("max = " + max);
+    }
+}
