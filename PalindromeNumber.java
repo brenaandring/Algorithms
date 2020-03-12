91f5659cffe9ba260f2afefa31c7f8d34b75819a
Brena Andring <bzandring@gmail.com>
2020-03-11 22:38:16 -0700

    leetcode algorithm

---
+++ PalindromeNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        return x == reverse(x);
+    }
+
+    public static int reverse(int x) {
+        int reversedX = 0;
+        try {
+            while (x != 0) {
+                int digit = x % 10;
+                //System.out.println("res before: " + reversedX);
+                reversedX = Math.addExact(Math.multiplyExact(reversedX, 10), digit);
+                //System.out.println("res after: " + reversedX + ", just added: " + digit);
+                x = x / 10;
+            }
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+        return reversedX;
+    }
+}
