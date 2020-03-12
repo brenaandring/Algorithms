59a5a0a3ea0930b3acb06e9f3a4b818f8688bec7
Brena Andring <bzandring@gmail.com>
2020-03-11 22:35:08 -0700

    leetcode algorithm

---
+++ ReverseInteger.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int reversedX = 0;
+        try {
+            while (x != 0) {
+                int digit = x % 10;
+                //System.out.println("res before: " + reversedX);
+                reversedX = Math.addExact(Math.multiplyExact(reversedX, 10), digit);
+                //System.out.println("res after: " + reversedX + ", just added: " + digit);
+                x = x / 10;
+            }
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+        return reversedX;
+    }
+}
