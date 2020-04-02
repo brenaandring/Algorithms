19b0617fd4caec7012aa3ffba7cebcacde9aa10c
Brena Andring <bzandring@gmail.com>
2020-04-02 14:21:08 -0700

    30-day leetcoding challenge Happy Number

---
+++ HappyNumber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        while (!hashSet.contains(n)) {
+            hashSet.add(n);
+            n = findSum(n);
+            if (n == 1)
+                return true;
+        }
+        return false;
+    }
+
+    public int findSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
